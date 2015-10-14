6f5049fc1f5ce03dbf0e85bec2565dec37f5612b
jdrew5 <jdrew5@hotmail.com>
2015-10-14 18:35:49 -0500

    Initial commit

---
+++ solo_script.js
@@ -0,0 +1,92 @@
+// ! ! !
+// Three Bugs
+// Bug 1: calling calculateSTI(array[i]) was missing the [i], thus it was passing the same array in each time.
+// Bug 2: added Math.round to the output array to round adjusted annual compensation and total bonus values
+// Bug 3: getBaseSTI was returning percent - 1.  Should just return percent.
+// Hard mode: added .join(" ") to array output to have spaces instead of ,
+// Pro mode: set attribute on the ul element identified by 'content' to get rid of bullets
+
+var arrayAtticus = ["Atticus", "2405", "47000", 3];
+var arrayJem = ["Jem", "62347", "63500", 4];
+var arrayBoo = ["Boo", "11435", "54000", 3];
+var arrayScout = ["Scout", "6243", "74750", 5];
+
+var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+
+//Create variables used to write to the DOM
+var newEl, newText, position;
+//Capture the position of insertion into the DOM
+position = document.getElementById('content');
+// get rid of bullets by adding attribute to ul tag.
+document.getElementById('content').setAttribute("style","list-style: none;");
+
+//Loop the array, extracting each array and writing information to the DOM
+//Note that the information is not 'clean'
+for(var i = 0; i < array.length; i++){
+	array[i] = calculateSTI(array[i]); // need to increment array within the calculateSTI(array) - added [i]
+ 	newEl = document.createElement('li');
+	newText = document.createTextNode(array[i].join(" "));
+	newEl.appendChild(newText);
+	position.appendChild(newEl);
+}
+
+function calculateSTI(array){
+  var newArray = [];
+
+  newArray[0] = array[0];
+
+  var employeeNumber = array[1];
+  var baseSalary = array[2];
+  var reviewScore = array[3];
+
+  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+  if(bonus > 0.13){
+    bonus = 0.13;
+  }
+
+  newArray[1] = bonus;
+  newArray[2] = Math.round(baseSalary * (1.0 + bonus)); //added Math.round
+  newArray[3] = Math.round(baseSalary * bonus); //added Math.round
+  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
+  return newArray;
+}
+
+function getBaseSTI(reviewScore){
+  var basePercent;
+  switch(reviewScore){
+    case 1:
+      basePercent = 0;
+      break;
+    case 2:
+      basePercent = 0;
+      break;
+    case 3:
+      basePercent = 0.04;
+      break;
+    case 4:
+      basePercent = 0.06;
+      break;
+    case 5:
+      basePercent = 0.10;
+      break;
+  }
+//  return basePercent - 1; (old code)
+    return basePercent;
+}
+
+function getYearAdjustment(employeeNumber){
+  var yearAdjustment = 0;
+  if(employeeNumber.length == 4){
+    yearAdjustment = 0.05;
+  }
+  return yearAdjustment;
+}
+
+function getIncomeAdjustment(salary){
+  var incomeAdjustment = 0;
+  salary = parseInt(salary);
+  if(salary > 65000){
+    incomeAdjustment = 0.01;
+  }
+  return incomeAdjustment;
+}
